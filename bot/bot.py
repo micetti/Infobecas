48028ea326b59f2350ec9e855cedbadd38f36739
Enrique Saez <enrique.saez@skyscanner.net>
2017-05-20 20:48:21 +0200

    Add custom keyboard to predefined answers

--- bot/bot.py
+++ bot/bot.py
@@ -4,11 +4,11 @@ from telegram.ext import Updater, CommandHandler
 from telegram.ext import MessageHandler, Filters
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 
-from answers.predefined_answers import PredefinedAnswers
+from answers.predefined_answers import predefined_answers
 
 import user
 from questions import questions
-from token import token
+from token_file import token
 
 redis = Redis(host='127.0.0.1', port=6379, db=0)
 
@@ -16,21 +16,23 @@ TOKEN = token
 
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=PredefinedAnswers.welcome_message)
+    bot.send_message(chat_id=update.message.chat_id, text=predefined_answers.welcome_message)
 
 
 def echo(bot, update):
     chat_id = update.message.chat_id
+
     user_profile = redis.get(chat_id)
 
     if user_profile:
+
         content = update.message.text
         returning_user = user.create_user_from_json(chat_id, user_profile.decode("utf-8"))
         returning_user.answer_question(content)
         next_question = returning_user.next_question()
         returning_user.last_question = next_question
         if next_question:
-            bot.send_message(chat_id, questions[next_question])
+             dumb_reply(bot, chat_id, next_question)
         else:
             bot.send_message(chat_id=chat_id, text="Thats all!")
         redis.set(chat_id, returning_user.get_as_json())
@@ -39,8 +41,16 @@ def echo(bot, update):
         first_time_user = user.User(chat_id)
         next_question = first_time_user.next_question()
         first_time_user.last_question = next_question
-        bot.send_message(chat_id=chat_id, text=questions[next_question])
+        dumb_reply(bot, chat_id, next_question)
         redis.set(chat_id, first_time_user.get_as_json())
+
+
+def dumb_reply(bot, chat_id, next_question):
+    if next_question in predefined_answers:
+        bot.send_message(chat_id=chat_id, text=questions[next_question],
+                         reply_markup=ReplyKeyboardMarkup(predefined_answers[next_question], one_time_keyboard=True))
+    else:
+        bot.send_message(chat_id=chat_id, text=questions[next_question])
 
 
 if __name__ == '__main__':
--- bot/questions.py
+++ bot/questions.py
@@ -1,8 +1,12 @@
 questions = {
-'age' : 'How old are you?', 
+'age' : 'How old are you?',
 'name': 'Hey! What’s your name',
 'gender': 'Whats your gender?',
 'nationality': 'What is your nationality?',
 'email': 'Could you tell me your email address?',
-'recidency': 'Which is your country of residence?'
+'recidency': 'Which is your country of residence?',
+'academic_degree': 'Whats your highest academic degree?',
+'scholarship_type': 'Which kind of scholarship are you interested in?',
+'language_skills': 'What are your language skills?',
+'financial_status': 'What is your current financial situation?'
 }
