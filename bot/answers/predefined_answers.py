9b1b30456573e7eb4268adaf2fdc8cf812909feb
Enrique Saez <enrique.saez@skyscanner.net>
2017-05-20 18:16:02 +0200

    Add predefined answers

---
+++ bot/answers/predefined_answers.py
@@ -0,0 +1,9 @@
+class PredefinedAnswers:
+
+    welcome_message = "Hello, I'll help you find some grants"
+    numbers = [['7', '8', '9'], ['4', '5', '6'], ['1', '2', '3'], ['0']]
+    academic_degree = [['Higher education'], ['University degree'], ['Master']]
+    scholarship_type = [['Financial aid'], ['Scholarships'], ['Investigation funding']]
+    boolean_question = [['Yes'], ['No']]
+    language_skills = [['English'], ['French'], ['Spanish']]
+    financial_status = [['Sufficient'], ['No own income'], ['Financial need'], ['High financial need'], ['Financial emergency situation']]
