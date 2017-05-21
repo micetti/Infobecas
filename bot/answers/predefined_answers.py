364a4c48936bca054cce924fd159f8e3d1d7b22c
Michael Petretti <michael.petretti@skyscanner.net>
2017-05-21 11:30:09 +0200

    make it talk more like a human

--- bot/answers/predefined_answers.py
+++ bot/answers/predefined_answers.py
@@ -3,8 +3,13 @@ from telegram import Emoji
 woman = Emoji.WOMAN
 man = Emoji.MAN
 undefined = Emoji.BLACK_QUESTION_MARK_ORNAMENT
+waving_hand = Emoji.WAVING_HAND_SIGN
+hourglass = Emoji.HOURGLASS
 
-welcome_message = "Hello, I'll help you find some grants"
+welcome_message = "Welcome to the Infobeca bot"
+starting_message = "Hey there " + waving_hand + "!\nI am here to help you find a Grant.\nYou will see, it is riddiculosly easy."
+start_explanation = "We are now going to have a little chat, so that I know just enough to help you. Get ready, first question:"
+search_for_match = hourglass + "Hold on.\nI will now search for a match."
 
 predefined_answers={
 'academic_degree': [['Higher education'], ['University degree'], ['Master']],
@@ -14,5 +19,4 @@ predefined_answers={
 'financial_status': [['Sufficient'], ['No own income'], ['Financial need'], ['High financial need'], ['Financial emergency situation']],
 'gender': [[woman], [man], [undefined]],
 'big_family': [['Yes'], ['No']],
-
 }
--- bot/bot.py
+++ bot/bot.py
@@ -5,6 +5,7 @@ from telegram.ext import MessageHandler, Filters
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove, ParseMode)
 
 from answers.predefined_answers import predefined_answers
+import answers.predefined_answers as phrase
 
 import user
 from questions import questions
@@ -16,7 +17,7 @@ redis = Redis(host='127.0.0.1', port=6379, db=0)
 TOKEN = token
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=predefined_answers.welcome_message)
+    bot.send_message(chat_id=update.message.chat_id, text=phrase.welcome_message)
 
 def run(bot, update):
     chat_id = update.message.chat_id
@@ -39,8 +40,8 @@ def build_nice_response(match):
     budget = match['budget']
     num_scholarships = match['num_scholarships']
     response = '<b> ' + name + '</b>\n'
-    response += 'Number of Scholarships: ' + str(num_scholarships) + '\n'
-    response += 'Volume of the Scholarship' + str(budget) + '\n'
+    response += 'They are offering this Scholarship to ' + str(num_scholarships) + ' people.\n'
+    response += 'In total it has a value of' + str(budget) + ' Euro.\n\n'
     response += 'Talk to them here: <a href="http://google.com">link</a>'
     return response
 
@@ -57,18 +58,32 @@ def get_user(chat_id, bot):
     if user_profile:
         return user.create_user_from_json(chat_id, user_profile.decode("utf-8"))
     else:
-        bot.send_message(chat_id=chat_id, text="Hello, nice to see you here.\nLets have a little chat to find you a match")
+        bot.send_message(chat_id=chat_id, text=phrase.starting_message)
+        time.sleep(2)
+        bot.send_message(chat_id=chat_id, text=phrase.start_explanation)
+        time.sleep(1)
         return user.User(chat_id)
 
 def finalize_conversation(chat_id, bot, user_profile):
-        bot.send_message(chat_id=chat_id, text="Thats all! Here is the summay of our nice conversation:")
+        bot.send_message(chat_id=chat_id, text="Thats all! Here is what I understood from our chat:")
         bot.send_message(chat_id=chat_id, text=user_profile.get_summary_message())
-        match = user_profile.get_match()
         time.sleep(3)
+        bot.send_message(chat_id=chat_id, text="If you find some thing wrong, you can start over by typing /delete")
+        time.sleep(1)
+        bot.send_message(chat_id=chat_id, text=phrase.search_for_match)
+        match = user_profile.get_match()
+        time.sleep(2)
+        bot.send_message(chat_id=chat_id, text=phrase.hourglass + "\nProtipp: A recomendation letter can make the difference")
+        time.sleep(2)
+        bot.send_message(chat_id=chat_id, text=phrase.hourglass + "\nDid you know that last year 1.9B dollar in scholaships did not match a candidate")
+        time.sleep(2)
+        bot.send_message(chat_id=chat_id, text=phrase.hourglass + "\nProtipp: Make sure you know about the institution you are about to apply to")
+        time.sleep(2)
         if not match:
             bot.send_message(chat_id=chat_id, text="Sorry no matches found")
         else:
-            bot.send_message(chat_id=chat_id, text="We found a match for you")
+            bot.send_message(chat_id=chat_id, text="\nWe found a match for you\n")
+            time.sleep(2)
             nice_match_found_message = build_nice_response(institutions[0])
             bot.send_photo(chat_id=chat_id, photo='http://pm1.narvii.com/6415/ac702663901e3934c297213aabaec4204ae6a106_128.jpg')
             bot.send_message(chat_id=chat_id, text=nice_match_found_message, parse_mode=ParseMode.HTML)
